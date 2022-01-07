b927af543e2fb365d1dd067b4fe543e21276df8d
chittrakumari <chittrakumari18@gmail.com>
2022-01-07 15:30:15 +0530

    first commit

---
+++ articles/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    #for small sized text we use char feild.
+    title = models.CharField(max_length=100)
+    
+    # feild used by django used to store and generate valid urls
+    slug =models.SlugField()
+    
+    body = models.TextField()
+    
+    #auto_now_add=automatically populates the time feild when the article is created
+    date=models.DateTimeField(auto_now_add=True)
+    
+    
+    
+    #how the objects would look if we retrive them i.e through titles
+    def __str__(self):
+        return self.title;
+        
