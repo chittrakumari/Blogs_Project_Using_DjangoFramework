2c3695970db5c35e51e5dc33387bdf15d53f04f3
chittrakumari <chittrakumari18@gmail.com>
2022-01-07 16:10:07 +0530

    model methods

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from  .models import Article
 def article_list(request):
-    return render(request,'articles/article_list.html')
+    
+    articles=Article.objects.all().order_by('date')
+    
+    #we are passing a third parameter articles using dictionary or key-value pair 
+    return render(request,'articles/article_list.html',{'articles':articles})
 # Create your views here.
